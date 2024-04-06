cb03ea49c1c22b24580548baf8e3ae83c59707d0
Arish5Kannan <arishkannan40@gmail.com>
2024-04-05 21:44:35 -0700

    third commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var output = document.getElementById("text-box");
+
+    function display(num){
+        output.value += num; 
+    }
+    function Clear()
+    {
+        document.getElementById("text-box").value="";
+    }
+    function calculate()
+    {
+        try{
+            if(output.value === "")
+            {
+                output.value = "Syntax Error";
+            }
+            else{
+            output.value = eval(output.value);
+            }
+        }
+        catch(err){
+            output.value="Invalid input";
+        }
+
+    }
+    function Delete()
+    {
+        output.value = output.value.slice(0,-1);
+    }
